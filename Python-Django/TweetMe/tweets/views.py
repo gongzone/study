a5b1f589d3b063a68993aefa31cb3e02170e44d4
GongZone <dnjsqhwo@gmail.com>
2022-01-16 00:50:38 +0900

    공부 업데이트

--- Python-Django/TweetMe/tweets/views.py
+++ Python-Django/TweetMe/tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
@@ -7,6 +8,16 @@ from .models import Tweet
 
 def home_view(request, *args, **kwargs):
   return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+  qs = Tweet.objects.all()
+  tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 125)} for x in qs]
+  data = {
+    "isUser": False,
+    "response": tweets_list
+  }
+
+  return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
   """
