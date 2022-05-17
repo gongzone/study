9a5ba58e7ed2239375dcd8aba47b85bc08d91460
dnjsqhwo <dnjsqhwo@gmail.com>
2022-05-18 01:16:45 +0900

    feat: 공부 업데이트

---
+++ web-components/modal-component/modal.js
@@ -0,0 +1,73 @@
+class Modal extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        #backdrop {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100vh;
+          background-color: rgba(0,0,0,0.75);
+          z-index: 10;
+        }
+
+        #modal {
+          position: fixed;
+          top: 15vh;
+          left: 25%;
+          width: 50%;
+          height: 20rem;
+          z-index: 100;
+          background-color: white;
+          border-radius: 3px;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+
+        #main {
+          padding: 1rem;
+        }
+
+        header {
+          padding: 1rem;
+        }
+
+        header h1 {
+          font-size: 1.25rem;
+        }
+
+        #actions {
+          border-top: 1px solid #ccc;
+          padding: 1rem;
+          display: flex;
+          justify-content: flex-end;
+        }
+
+        #actions button {
+          margin: 0 0.25rem;
+        }
+
+      </style>
+      <div id="backdrop"></div>
+      <div id="modal">
+        <header>
+          <h1>Please Confirm</h1>
+        </header>
+        <section id="main">
+          <slot></slot>
+        </section>
+        <section id="actions">
+          <button>Cancel</button>
+          <button>Okay</button>
+        </section>
+      </div>
+    `;
+  }
+}
+
+customElements.define('uc-modal', Modal);
