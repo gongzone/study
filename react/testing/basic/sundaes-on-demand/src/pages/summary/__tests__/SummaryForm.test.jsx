9d577e804c728172087ab61d4141ad2916521398
dnjsqhwo <dnjsqhwo@gmail.com>
2022-06-29 03:18:10 +0900

    update

--- react/testing/basic/sundaes-on-demand/src/pages/summary/__tests__/SummaryForm.test.jsx
+++ react/testing/basic/sundaes-on-demand/src/pages/summary/__tests__/SummaryForm.test.jsx
@@ -1,4 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import SummaryForm from '../SummaryForm';
 
 test('Initial conditions', () => {
@@ -14,15 +16,38 @@ test('Initial conditions', () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test('Checkbox disables button on first click and disables on second click', () => {
+test('Checkbox disables button on first click and disables on second click', async () => {
   render(<SummaryForm />);
+  const user = userEvent.setup();
 
   const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i });
   const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test('Popover responds to hover', async () => {
+  render(<SummaryForm />);
+  const user = userEvent.setup();
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
